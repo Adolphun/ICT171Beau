72666fbcdef78cb2bc3fadf508c3bb072d6cc3d4
Adolphun <169060008+Adolphun@users.noreply.github.com>
2024-05-29 12:51:10 +0800

    this was my latest change.

--- processform.php
+++ processform.php
@@ -1,3 +1,4 @@
+<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['taskIndex'])) {
         $taskIndex = $_POST['taskIndex'];
@@ -11,6 +12,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $taskName = $_POST['taskNameEntry'];
         $taskDuration = $_POST['taskDurationEntry'];
         $day = $_POST['dayEntry'];
+
+        // Validate input
+        if (!is_string($taskName) || empty($taskName)) {
+            echo "Invalid task name.";
+            return;
+        }
+        if (!is_numeric($taskDuration) || $taskDuration < 0 || $taskDuration > 24) {
+            echo "Invalid task duration.";
+            return;
+        }
+
+        // Check if task can be added
+        if ($_SESSION['totalHours'][$day] < $taskDuration) {
+            echo "Not enough hours remaining for this day.";
+            return;
+        }
+
         $_SESSION['totalHours'][$day] -= $taskDuration;
         // Add task to tasks list
         $_SESSION['tasks'][] = array('name' => $taskName, 'duration' => $taskDuration, 'day' => $day);
