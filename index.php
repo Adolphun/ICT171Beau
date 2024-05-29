042c0c5b2d566033d05419032832e61f83379598
Adolphun <169060008+Adolphun@users.noreply.github.com>
2024-05-29 12:45:20 +0800

    this is what giving up looks like

--- index.php
+++ index.php
@@ -3,17 +3,31 @@ session_start();
 
 include 'config.php';
 include 'displaycalendar.php';
-include 'processform.php';
 include 'addTask.php';
 include 'addLicenceFooter.php';
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 displaycalendar();
-proccessform()
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['taskNameEntry']) && isset($_POST['taskDurationEntry']) && isset($_POST['dayEntry'])) {
+    if (isset($_POST['taskIndex'])) {
+        $taskIndex = $_POST['taskIndex'];
+        // Remove the task from the tasks list
+        if (isset($_SESSION['tasks'][$taskIndex])) {
+            $removedTask = $_SESSION['tasks'][$taskIndex];
+            $_SESSION['totalHours'][$removedTask['day']] += $removedTask['duration'];
+            unset($_SESSION['tasks'][$taskIndex]);
+        }
+    } elseif (isset($_POST['taskNameEntry']) && isset($_POST['taskDurationEntry']) && isset($_POST['dayEntry'])) {
+        $taskName = $_POST['taskNameEntry'];
+        $taskDuration = $_POST['taskDurationEntry'];
+        $day = $_POST['dayEntry'];
+        $_SESSION['totalHours'][$day] -= $taskDuration;
+        // Add task to tasks list
+        $_SESSION['tasks'][] = array('name' => $taskName, 'duration' => $taskDuration, 'day' => $day);
+        $_SESSION['taskAdded'] = true;
+
         addTask();
     }
 }
--- processform.php
+++ processform.php
@@ -1,39 +1,19 @@
-<?php
-function proccessform() {
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST['taskIndex'])) {
-            $taskIndex = $_POST['taskIndex'];
-            // Remove the task from the tasks list
-            if (isset($_SESSION['tasks'][$taskIndex])) {
-                $removedTask = $_SESSION['tasks'][$taskIndex];
-                $_SESSION['totalHours'][$removedTask['day']] += $removedTask['duration'];
-                unset($_SESSION['tasks'][$taskIndex]);
-            }
-        } else {
-            $taskName = $_POST['taskNameEntry'];
-            $taskDuration = $_POST['taskDurationEntry'];
-            $day = $_POST['dayEntry'];
-
-            // check correct inputs 
-            if (!is_string($taskName) || empty($taskName)) {
-                echo "Invalid task name.";
-                return;
-            }
-            if (!is_numeric($taskDuration) || $taskDuration < 0 || $taskDuration > 24) {
-                echo "Invalid task duration.";
-                return;
-            }
-
-            // Check if task can be added negative hours are weird
-            if ($_SESSION['totalHours'][$day] < $taskDuration) {
-                echo "Not enough hours remaining for this day.";
-                return;
-            }
-
-            $_SESSION['totalHours'][$day] -= $taskDuration;
-            // Add task to tasks list
-            $_SESSION['tasks'][] = array('name' => $taskName, 'duration' => $taskDuration, 'day' => $day);
-            $_SESSION['taskAdded'] = true;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['taskIndex'])) {
+        $taskIndex = $_POST['taskIndex'];
+        // Remove the task from the tasks list
+        if (isset($_SESSION['tasks'][$taskIndex])) {
+            $removedTask = $_SESSION['tasks'][$taskIndex];
+            $_SESSION['totalHours'][$removedTask['day']] += $removedTask['duration'];
+            unset($_SESSION['tasks'][$taskIndex]);
         }
+    } else {
+        $taskName = $_POST['taskNameEntry'];
+        $taskDuration = $_POST['taskDurationEntry'];
+        $day = $_POST['dayEntry'];
+        $_SESSION['totalHours'][$day] -= $taskDuration;
+        // Add task to tasks list
+        $_SESSION['tasks'][] = array('name' => $taskName, 'duration' => $taskDuration, 'day' => $day);
+        $_SESSION['taskAdded'] = true;
     }
 }
