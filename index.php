c5118bdf8a0948f55cbaf0534015389118e3c497
Adolphun <169060008+Adolphun@users.noreply.github.com>
2024-05-29 12:39:01 +0800

    CAPITALS!!!

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ session_start();
 
 include 'config.php';
 include 'displaycalendar.php';
-include 'processForm.php';
+include 'processform.php';
 include 'addTask.php';
 include 'addLicenceFooter.php';
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 displaycalendar();
-processForm();
+processform();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['taskNameEntry']) && isset($_POST['taskDurationEntry']) && isset($_POST['dayEntry'])) {
