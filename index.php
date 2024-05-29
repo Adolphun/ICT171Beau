0ffd38af8d556c459d39d921dc2b1cee177ecd09
Adolphun <169060008+Adolphun@users.noreply.github.com>
2024-05-29 12:17:53 +0800

    this is why you never make changes.

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
+session_start();
+
 include 'config.php';
 include 'displayCalendar.php';
 include 'processForm.php';
 include 'addTask.php';
 include 'addLicenceFooter.php';
 
-session_start();
 displayCalendar();
-addTask();
+
+// Process the form if it was submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['taskNameEntry']) && isset($_POST['taskDurationEntry']) && isset($_POST['dayEntry'])) {
+        addTask();
+    }
+}
+
 addLicenceFooter();
